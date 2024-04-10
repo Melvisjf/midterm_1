fddbe6ceb4a1df716e25d76115a4185b5a79d50b
JU4N.F3 <juanjoflorez_@hotmail.com.com>
2024-04-09 18:16:34 -0700

    initial commit

---
+++ course/exc1.py
@@ -0,0 +1,15 @@
+#Slicing strings
+'''''
+s= "Monty Python"
+print (s[0:4]) #im telling the program to print since label 0 to 4
+# which is gonna be 'Mont', doesnt include the last one.
+
+print(s[:2]) # print until 2, so doesnt include it
+#will print 'Mo' 
+
+'''''
+answer = input("Are u Yana?: ")
+if answer== "yes":
+    print ("Hello There !")
+else:
+    print ("You are not, bye !")
